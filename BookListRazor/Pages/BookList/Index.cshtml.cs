7077c3b6c68930f8cae8820eb86bab08cfaf2599
kevinwong327 <kevin.wong1@gmail.com>
2019-11-25 00:20:27 -0500

    Sec 3:33-Index Get Handler

--- BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,13 @@ namespace BookListRazor.Pages.BookList
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
         public void OnGet()
         {
 
