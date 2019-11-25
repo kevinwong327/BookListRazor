295b485fb7d6e82ab55437bbbb90de55202159d0
kevinwong327 <kevin.wong1@gmail.com>
2019-11-24 20:43:01 -0500

    Sec 3:27-Add Model to Db

--- BookListRazor/Model/ApplicationDbContext.cs
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookListRazor.Model
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book>  Book { get; set; }
+
     }
 }
